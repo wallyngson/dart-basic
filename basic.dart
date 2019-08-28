0f8ce2cda9f5f36616a271f3e570d7d08b825b90
wallyngson <wallyngson.guedes@ccc.ufcg.edu.br>
2019-08-28 13:53:53 -0300

    Create basic.dart

---
+++ basic.dart
@@ -0,0 +1,23 @@
+// Aprendendo o básico do DART
+
+// 01. Outputs
+// 02. Classe Main()
+// 03. Variáveis
+
+main () {
+
+  print('Hello World');
+  print('Segunda impressão...'); // imprimindo string...
+  print(2 + 5); // inteiro...
+  print(2.5); // float...
+
+  var name = 'Hello World!'; // string
+  print(name); // output: Hello World!
+
+  name = 'Other name...';
+  print(name); // output: Other name...
+
+  var number = 2; // int
+  print(number); // output: 2
+  
+}
