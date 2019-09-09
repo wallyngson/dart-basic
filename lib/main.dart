2b951ad9ac76091477269c892b543afa0202203b
wallyngson <wallyngson.guedes@ccc.ufcg.edu.br>
2019-09-08 21:45:56 -0300

    product-cart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 import 'src/bmi_calculation.dart';
 import 'src/age_calculation.dart';
-import 'src/shopping_cart.dart';
+import 'src/product_cart.dart';
 
 main(List<String> arguments) {
   if (arguments[0] == 'age_calculation') {
     age();
   } else if (arguments[0] == 'bmi_calculation'){
     bmi();
-  } else if (arguments[0] == 'shopping_cart') {
+  } else if (arguments[0] == 'product_cart') {
     cart(); 
   } else {
     print('Ohhh sorry, enter a program valid...');
